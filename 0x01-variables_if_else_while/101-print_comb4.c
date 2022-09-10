3584375aeb072e53cdaf7b01c96ff6aa5e3bc9c2
titayi <kitayimike@gmail.com>
2022-09-09 19:09:44 -0700

    The success combination in business

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	int m, k, w;
+
+	for (m = '0'; m <= '9'; m++)
+	{	
+		for (k = '0'; k <= '9'; k++)
+		{
+			for (w = '0'; w = '9'; w++)
+			{
+				if (m < k && k < w)
+
+   				{
+					putchar(m);
+					putchar(k);
+					putchar(w);
+
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
