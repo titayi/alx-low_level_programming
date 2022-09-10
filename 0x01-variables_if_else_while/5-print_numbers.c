49814cb69e774ba5bac0163fc8450ac68ac1f356
titayi <kitayimike@gmail.com>
2022-09-09 17:36:09 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	char m;
+
+	for (m = '0'; m <= '9'; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+
+	return (0);
+}
