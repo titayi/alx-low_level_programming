31e7bad2837c0ec74b1e6454bda10cdc2a38fedb
titayi <kitayimike@gmail.com>
2022-09-11 23:31:02 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+
 int main(void)
 {
-	int m, n;
+	int m, k;
 
 	for (m = 0; m <= 98; m++)
 	{
-		for (n = m + 1; n <= 99; n++)
+		for (k = m + 1; k <= 99; k++)
 		{
 			putchar((m / 10) + '0');
 			putchar((m % 10) + '0');
-			putchar(' ');
-			putchar(n / 10) + '0');
-			putchar(n % 10) + '0');
+			putchar('\n');
+			putchar((k / 10) + '0');
+			putchar(k / % 10) + '0');
 
-			if (m == 98 && n == 99);
+			if(m == 98 && k == 99)
+
 				continue;
 
 			putchar(',');
 			putchar(' ');
 		}
 	}
-
-	putchar('\n');
 
 	return (0);
 }
