b58a26cb290b92b94b2e5ca65d7ce91c65d924b0
titayi <kitayimike@gmail.com>
2022-09-13 15:15:20 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints letters a - z
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
