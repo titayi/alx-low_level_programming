0f3018c88b8c8abaadb0bf39ee865c98849082f5
titayi <kitayimike@gmail.com>
2022-09-13 13:53:08 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,8 +5,17 @@
  *
  * Return: 0
  */
-int _putchar()
+int _putchar(void)
 {
-	write(1, "_putchar", 9);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
