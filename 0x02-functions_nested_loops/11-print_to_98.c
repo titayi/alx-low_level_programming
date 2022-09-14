641983c5243cdaf75567646fa8765fe226d49c03
titayi <kitayimike@gmail.com>
2022-09-14 06:59:11 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,13 +7,16 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 	{
-		for (n = n; n <= 97; n++)
-			_putchar(n);
-		}
-		else
-			for (n = n; n > 98; n--)
-				_putchar(n);
-	_putchar("98\n");
+		for (n = n; n < 98; n++)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
+	else
+	{
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
 }
