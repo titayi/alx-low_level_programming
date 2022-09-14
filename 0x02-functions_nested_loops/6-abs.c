c42e7671b0cfc870f0cd98da7a39ebaa9338d983
titayi <kitayimike@gmail.com>
2022-09-14 00:50:26 -0700

    The next tasks

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - func that computes the absolute value of an integer
+ *
+ * Return: 0
+ */
+int _abs(int)
+{
+	if (n < 0)
+	{
+		return (n);
+	} else
+	{
+		return (-n);
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function that prints the last digit of a number
+ *
+ * @n: the checked character
+ *
+ * Return: the last digit
+ */
+int print_last_digit(int n)
+{
+	n = n % 10;
+
+	if (n < 0)
+		n = -n;
+		_putchar(n + '0');
+	return (n);
+}
