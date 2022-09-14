f47939d7641078bde0c8ae3ec092c37f3d6010ff
titayi <kitayimike@gmail.com>
2022-09-14 06:41:23 -0700

    Task 7

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,10 +10,10 @@ void print_to_98(int n)
 	if (n <= 98)
 	{
 		for (n = n; n <= 97; n++)
-			printf("%d, ", n);
+			_putchar(n);
 		}
 		else
 			for (n = n; n > 98; n--)
-				printf("%d, ", n);
-		printf("98\n");
+				_putchar(n);
+	_putchar("98\n");
 }
