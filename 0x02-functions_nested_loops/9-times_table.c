0ecd4e21cce6f8d1f5bdda0d10f1f160e3dcb8b3
titayi <kitayimike@gmail.com>
2022-09-14 02:33:35 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 				_putchar((res / 10) + '0');
 				_putchar((res % 10) + '0');
 			}
-			else if (res < 10 && m !+ 0)
+			else if (res < 10 && m != 0)
 			{
 				_putchar(' ');
 				_putchar((res % 10) + '0');
