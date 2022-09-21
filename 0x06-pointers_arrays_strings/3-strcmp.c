76d0c7e0a79d1e7031131bd6b4a2e28f04750675
titayi <kitayimike@gmail.com>
2022-09-21 12:36:59 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,12 +8,29 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i = 0, d = 0;
 
-	while ((s1[i] == s2[i]) && (s1[i] != '\0'))
+	while (1)
 	{
-		i++;
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
+		{
+			d = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			d = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			d = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
 	}
-
-	return (s1[i] - s2[i])
+	return (d);
 }
