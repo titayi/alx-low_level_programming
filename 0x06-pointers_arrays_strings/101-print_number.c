1d51e15d062aa301453b20e8e2af598f981b87d3
titayi <kitayimike@gmail.com>
2022-09-22 06:37:53 -0700

    Hot fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,16 +7,27 @@
  */
 void print_number(int n)
 {
-	unsigned int m = n;
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
-		_putchar('-');
-		m = -m;
+		_putchar(45);
+		m = n * -1;
 	}
-	if ((m / 10) > 0)
+	else
 	{
-		print_number(m / 10);
+		m = n;
 	}
-	_putchar((m % 10) + '0');
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
