33681d686f30042591eecf0c3ac40459a7c990c0
titayi <kitayimike@gmail.com>
2022-09-22 06:18:14 -0700

    Magic fix

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * infinite_add - a function that adds two numbers
+ * @n1: first param
+ * @n2: second param
+ * @r: third param
+ * @size_r: the size
+ * Return: String
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i, j, k, l, m, n;
+
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
+		r
+}
